5775d61da966875df92188b270c94dc7179a2272
limaluan <limaluan.dev@gmail.com>
2022-03-23 20:32:49 -0300

    adds: List Categories useCase

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,17 +1,15 @@
 import { Router } from "express";
-import CategoriesRepository from "../modules/cars/repositories/CategoriesRepository";
 import createCategoryController from "../modules/cars/useCases/createCategory";
+import listCategoriesController from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
-const categories = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
     return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
-    const allCategories = categories.list();
-    return response.json(allCategories);
+    return listCategoriesController.handle(request, response);
 });
 
 export default categoriesRoutes;
